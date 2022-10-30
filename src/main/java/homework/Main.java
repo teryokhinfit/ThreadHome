f9689127b91a50988d4e37f17ad4a2fc68cb0680
teryokhinfit <terekhincw@gmail.com>
2022-10-30 22:18:52 +0400

    Initial commit

---
+++ src/main/java/homework/Main.java
@@ -0,0 +1,60 @@
+package homework;
+
+import java.util.*;
+
+public class Main {
+    // в среднем работа составила 50-70 сек, против 1.5-3 минут изначально!
+    public static void main(String[] args) throws InterruptedException {
+
+        String[] texts = new String[25];
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+
+        for (String text : texts) {
+            Runnable logic = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            };
+            Thread threadLog = new Thread(logic);
+            threads.add(threadLog);
+            threadLog.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        }
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
